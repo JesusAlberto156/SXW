fdd7644f4d78185895ecca9d10b1bd855edb0983
JesusAlberto156 <chuy492001@gmail.com>
2024-11-02 19:28:18 -0700

    Actualizaciones de archivos

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,9 +4,8 @@ export default {
     HOST: process.env.HOST || 'NO ENCONTRO VAR ENTORNO',
     PORT: process.env.PORT || 3000,
     API_URL: process.env.API_URL || '/TEC/TEPIC',
-    //CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb://partida04:Legendario156@localhost:27020/Estudiantes',
-    CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb+srv://partida04:Legendario156@tec-tepic.zvz5a.mongodb.net/',
-    DATABASE: process.env.DATABASE || 'Estudiantes',
+    CONNECTION_STRING: process.env.CONNECTION_STRING || 'mongodb+srv://JPartida04:Legendario156@sxw.r4yip.mongodb.net/',
+    DATABASE: process.env.DATABASE || 'db_sonido_xaviers',
     DB_PASSWORD: process.env.DB_PASSWORD || 'Legendario156',
-    DB_USER: process.env.DB_USER || 'partida04',
+    DB_USER: process.env.DB_USER || 'JPartida04',
 }
--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -4,8 +4,6 @@ import config from './config';
 (async () => {
     try {
         const db = await mongoose.connect(config.CONNECTION_STRING, {
-            //useNewUrlParser: true,
-            //useUnifiedTopology: true,
             dbName: config.DATABASE
         });
         console.log('Conectado a la Base de Datos: ', db.connection.name);
